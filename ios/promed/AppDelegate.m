4162324154d253df1139bc1da686e5f55e3cf3fb
alejandroledesma <alexledesmarayo@gmail.com>
2019-02-27 13:04:14 +0100

    Fix storybook

--- ios/promed/AppDelegate.m
+++ ios/promed/AppDelegate.m
@@ -22,7 +22,7 @@
                                                       moduleName:@"promed"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [UIColor blackColor];
+  rootView.backgroundColor = [UIColor whiteColor];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
