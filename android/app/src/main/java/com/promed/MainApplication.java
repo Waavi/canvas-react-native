da1006c2933c5443c77866a56440732628eed1ad
alejandroledesma <alexledesmarayo@gmail.com>
2019-02-28 10:39:23 +0100

    Install react-native-linear-gradient

--- android/app/src/main/java/com/promed/MainApplication.java
+++ android/app/src/main/java/com/promed/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import com.reactcommunity.rnlanguages.RNLanguagesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCWebViewPackage(), new RNLanguagesPackage(), new VectorIconsPackage(), new RNGestureHandlerPackage()
+            new RNCWebViewPackage(), new RNLanguagesPackage(), new VectorIconsPackage(), new RNGestureHandlerPackage(), new LinearGradientPackage()
       );
     }
 
