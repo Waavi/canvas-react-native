1690393312a84e82d98ced3af60a9ac8de65fde8
alejandroledesma <alexledesmarayo@gmail.com>
2019-02-28 10:39:23 +0100

    Install react-native-linear-gradient

--- src/config/app.js
+++ src/config/app.js
@@ -13,7 +13,7 @@ import {
 	REDUX_PERSIST_VERSION,
 	REDUX_PERSIST_ACTIVE,
 	STORYBOOK_SHOW,
-} from 'react-native-dotenv' /* ___timestamp___2019-02-22___11:31:22___ */
+} from 'react-native-dotenv' /* ___timestamp___2019-02-28___10:38:14___ */
 
 const webUrl = WEB_URL
 
