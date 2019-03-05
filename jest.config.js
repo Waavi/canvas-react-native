dad107c8792b6ea4bfda3946d3ad6f843b7d32ef
alejandroledesma <alexledesmarayo@gmail.com>
2019-03-05 16:27:55 +0100

    Added UI tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	preset: 'react-native',
 	testPathIgnorePatterns: ['<rootDir>/node_modules'],
-	setupFiles: ['./jest-setup.js'],
+	// setupFiles: ['./jest-setup.js'],
+	setupFilesAfterEnv: ['./jest-setup.js'],
 	moduleFileExtensions: ['js', 'jsx', 'json'],
 	testMatch: ['**/src/**/*.spec.(js|jsx|ts|tsx)'],
+	snapshotSerializers: ['enzyme-to-json/serializer'],
 }
